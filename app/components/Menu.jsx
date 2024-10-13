9d3a0c9af80235f95f16caba0431f222b72d399c
sohelkh1211 <khansohailabdussalam207@gmail.com>
2024-10-13 14:22:27 +0530

    Deploy errors resolved

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -2,12 +2,25 @@
 import { React, useState } from "react"
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import { createTheme, ThemeProvider } from '@mui/material'; // To config MUI icons breakpoints
 
 const Menu = () => {
     const [show, setShow] = useState(false);
+
+    const theme = createTheme({
+        breakpoints: {
+            values: {
+                xs: 0,
+                sm: 640,
+                md: 768,
+                lg: 1024
+            }
+        }
+    });
+
     return (
         <>
-            {!show ? <MenuIcon className="xs:flex sm:hidden text-white" onClick={() => setShow(!show)} /> : <CloseIcon className="xs:flex sm:hidden text-white" onClick={() => setShow(!show)} />}
+            {!show ? <ThemeProvider theme={theme}><MenuIcon sx={{ display: { md: 'none', xs: 'flex' } }} className="text-white" onClick={() => setShow(!show)} /></ThemeProvider> : <ThemeProvider theme={theme}><CloseIcon sx={{ display: { md: 'none', xs: 'flex' } }} className="text-white" onClick={() => setShow(!show)} /></ThemeProvider> }
 
             <div className={`xs:absolute md:hidden ${show ? 'flex flex-col' : 'hidden'} bg-white mt-[24.3rem] w-full left-0`}>
                 <ul className="list-none gap-x-8 text-black items-center">
